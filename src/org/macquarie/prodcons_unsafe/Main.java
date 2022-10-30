9ff1aeebb4214b88ae8ee1a110b643b6dede14c2
Damian Jurd <damian.jurd@mq.edu.au>
2022-10-30 13:41:59 +1100

    added mattr's non-thread safe version of producer/consumer

---
+++ src/org/macquarie/prodcons_unsafe/Main.java
@@ -0,0 +1,17 @@
+package org.macquarie.prodcons_unsafe;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BoundedBuffer<Integer> vBuffer = new BoundedBuffer<Integer>();
+		
+		// Create consumer and producer threads which talk with that buffer
+		Producer vProducer = new Producer(vBuffer);
+		Consumer vConsumer = new Consumer(vBuffer);
+		
+		// And start them....
+		vProducer.start();
+		vConsumer.start();
+	}
+
+}
